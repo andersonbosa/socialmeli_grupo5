521b90654239ff00dba0a57c992c4fa1cad804d1
Kevin Rafael Velez Bernal <kvbernal@br000cdqmn6jvqx.ml.com>
2025-01-21 17:30:45 -0300

    validated

--- src/main/java/com/bootcamp/socialmeligrupo5/controller/PostController.java
+++ src/main/java/com/bootcamp/socialmeligrupo5/controller/PostController.java
@@ -4,12 +4,14 @@ import com.bootcamp.socialmeligrupo5.dto.PostDTO;
 import com.bootcamp.socialmeligrupo5.service.PostService;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Validated
 @RequestMapping("/products")
 public class PostController {
 
