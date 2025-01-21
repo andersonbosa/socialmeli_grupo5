bc3638d7b0515b774b81fe379496c3ee4e0e24aa
andersonbosa <andersonbosa0@gmail.com>
2025-01-21 13:29:32 -0300

    feat: add JSON data loading for posts and include Jackson dependency

--- src/main/java/com/bootcamp/socialmeligrupo5/repository/InMemoryPostRepository.java
+++ src/main/java/com/bootcamp/socialmeligrupo5/repository/InMemoryPostRepository.java
@@ -1,8 +1,14 @@
 package com.bootcamp.socialmeligrupo5.repository;
 
 import com.bootcamp.socialmeligrupo5.entity.Post;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.ResourceUtils;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,8 +16,28 @@ import java.util.List;
 public class InMemoryPostRepository implements PostRepository {
 		public List<Post> posts = new ArrayList<>();
 
+    public InMemoryPostRepository() {
+        loadDatabase();
+    }
+
 		@Override
 		public void create(Post post) {
 				posts.add(post);
 		}
+
+    private void loadDatabase() {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+
+            // support Java 8 date time apis
+            objectMapper.registerModule(new JavaTimeModule());
+
+            File file = ResourceUtils.getFile("src/main/resources/posts.json");
+            List<Post> newPosts = objectMapper.readValue(file, new TypeReference<List<Post>>() {
+            });
+            posts.addAll(newPosts);
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
 }
